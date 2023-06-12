72bb76953969f1f16c4113bdf2928e7fd8b85e83
Shinon <contakt@shinon71.moe>
2023-06-13 06:32:32 +0800

    Fixes for kohya_ss-installer

--- kohya_ss-installer.sh
+++ kohya_ss-installer.sh
@@ -9,7 +9,7 @@ add-apt-repository -y ppa:deadsnakes/ppa
 apt update
 
 # Install required packages
-apt install -y python3.10 python3.10-tk python3.10-distutils python3.10-dev firefox git chromium-browser
+apt install -y python3.10 python3.10-tk python3.10-distutils python3.10-dev
 
 # Set Python 3.10 as the default Python interpreter
 update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 1
@@ -23,13 +23,15 @@ source ~/.bashrc
 export PATH="$HOME/.local/bin:$PATH"
 
 # Clone the repository
-git clone https://github.com/bmaltais/kohya_ss.git
+git clone https://github.com/kohya-ss/sd-scripts.git
 
 # Install dependencies
-cd kohya_ss
-python3.10 -m pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
+cd sd-scripts
+python3.10 -m pip install torch==1.13.1 torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
 python3.10 -m pip install --use-pep517 --upgrade -r requirements.txt
-python3.10 -m pip install xformers
+# commenting out xformers because you should really just train in fp32 instead.
+# Also, xformers tried to install pytorch 2 which is not what we want.
+# python3.10 -m pip install xformers
 
 # Check if the system has an NVIDIA A5000 GPU
 if nvidia-smi --query-gpu=name --format=csv,noheader | grep -q "A5000"; then
@@ -40,17 +42,12 @@ if nvidia-smi --query-gpu=name --format=csv,noheader | grep -q "A5000"; then
 
   # Reinstall torchvision
   python3.10 -m pip install --no-cache-dir --force-reinstall torchvision
+  # commenting out xformers because you should really just train in fp32 instead.
+  # Also, xformers tried to install pytorch 2 which is not what we want.
   python3.10 -m pip install xformers
 
   # Adjust TensorFlow, CUDA, and cuDNN installation if needed
 fi
-
-# Create a launcher script for the app
-launcher_script="../kohya_launcher.sh"
-echo "#!/bin/bash" > $launcher_script
-echo "cd $(pwd)" >> $launcher_script
-echo "python3.10 kohya_gui.py" >> $launcher_script
-chmod +x $launcher_script
 
 # Uninstall the system psutil package
 sudo apt remove -y python3-psutil
@@ -61,7 +58,6 @@ python3.10 -m pip install --upgrade psutil
 # Print a message to the user indicating the installation is complete
 echo "Kohya SS has been installed. To launch the app, close this window and open terminal then run accelerate config."
 echo "Accelerate config answers: this machine, no distributed training, NO, NO, NO, all, fp16."
-echo "Then run app using kohya_launcher.sh script."
-echo "Use python3.10"
+echo "python3.10 (python3) has been installed as well."
 # Exit the script
 exit 0
